c2cec2a3dcf8544e48c34407203532e2467e4cc7
gaeguriwadukerbi <gaeguriwadukerbi@gmail.com>
2024-09-12 17:29:39 +0900

    Add files via upload

---
+++ domlab/src/index.js
@@ -0,0 +1,14 @@
+console.log("test")
+const h1El = document.createElement("h1")
+heE1.textContent - "타이틀이 추가되었습니다."
+const heElParent = document.getElementById("root");
+console.log(heElParent)
+h1E1Parent.appendChild(h1El)
+
+const divEl = docment.crearteElment('div')
+divEl.className = 'container'
+
+const pEl = document.createElement('p')
+pE1.textContent = "영역 4입니다."
+
+console.log(pEl)
